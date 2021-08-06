84e71c1b7869be3d9211a14abc9a126bff0173dc
abhishekupadhyay260 <47971436+abhishekupadhyay260@users.noreply.github.com>
2021-08-07 00:00:10 +0530

    Add files via upload

---
+++ playsong.py
@@ -0,0 +1,4 @@
+from playsound import playsound
+playsound('D:\\Ringtones\\Hawayein - Jab Harry Met Sejal (Arijit)_1.mp3') #address of song followed by song name
+# we use double slash because single slash means escape sequence   
+playsound('D:\\Ringtones\\Aaina song from the body.mp3')
